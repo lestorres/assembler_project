31c4d1fedaa5ec800e030d9c302f40e43918ffd4
lesmes <lesmestorres@estudiantec.cr>
2025-03-17 21:49:26 -0600

    fetch_config_pila_completado

--- fetch_config/fetch_config.asm
+++ fetch_config/fetch_config.asm
@@ -1,4 +1,4 @@
-;Fetch config
+;Recuperar datos
 ;Lesmes Torres Gonzalez
 
 ;-----------------------------------
@@ -6,177 +6,97 @@
 %include "Linux64.inc"    ;Operaciones de impresion y cierra de archivo 
 %include "syscalls.inc" ;Compilacion de llamadas al sistema 
 
-; Sección de datos
+; Sección de datos inicializados
 section .data
-    archivo_config db "configuracion.txt", 0
-    var_fetch_config db 0, 0 ; contador de variables se crea en 0
-
-    ; Variables de recuperación
-    nota_apro db 3
-    nota_repo db 3
-    tam_grupo db 3
-    escala db 3
-    orden db 3
+    	archivo_config db "configuracion.txt", 0
 
 ; Sección de datos no inicializados
 section .bss
-    leer resb 4   ; variable de 4 bytes para los datos leídos
+    	leer_texto_config resb 154 ; variable para leer el texto_config   
+    
+; Variables de recuperación
+   	nota_apro resb 3
+    	nota_repo resb 3
+    	tam_grupo resb 3
+    	escala resb 3
+    	orden resb 1
+
+
 
 ; Sección de código
 section .text
-    global _start
+    	global _start
 
 _start:
-    ; Abrir archivo configuracion.txt
+    
+; Abrir archivo configuracion.txt
 apertura:
-    mov rax, SYS_OPEN    ; Llamada al sistema para abrir el archivo
-    mov rdi, archivo_config ; Nombre del archivo
-    mov rsi, O_RDONLY    ; Solo lectura
-    mov rdx, 0           ; No flags adicionales
-    syscall
+    	mov rax, SYS_OPEN    ; Llamada al sistema para abrir el archivo
+    	mov rdi, archivo_config ; Nombre del archivo
+    	mov rsi, O_RDONLY    ; Solo lectura
+    	mov rdx, 0           ; No flags adicionales
+    	syscall
 
-    ; Verificación de apertura del archivo
-    test rax, rax        ; Verificar si la apertura fue exitosa
-    js  salida_error     ; Salir si hubo un error
-    mov rbx, rax         ; Guardar descriptor del archivo
+; Verificación de apertura del archivo
+    	test rax, rax        ; Verificar si la apertura fue exitosa
+    	js  salida_error     ; Salir si hubo un error
+    	mov rbx, rax         ; Guardar descriptor del archivo
 
-; Bucle de lectura de configuracion.txt
-bucle_lectura_byte:
-    mov rdi, rbx         ; Cargar el descriptor del archivo
-    mov rax, SYS_READ    ; Llamada para leer un byte
-    mov rsi, leer        ; Dirección de la variable para leer
-    mov rdx, 1           ; Leer 1 byte
-    syscall	
-
-    ; Verificación de lectura de bytes
-    test rax, rax        ; Verificar si se leyeron bytes
-    jz  cerrar_archivo   ; Salir si no se leyeron bytes (fin de archivo)
+;-------------------------------------------------------------------
     
-    ; Verificar si se leyó ":"
-    mov al, byte[leer]
-    cmp al, ":"
-    jne bucle_lectura_byte ; NO es ":"
+lectura_texto_config:
+    	mov rdi, rbx                  ; Cargar el descriptor del archivo
+    	mov rax, SYS_READ             ; Llamada para leer
+    	mov rsi, leer_texto_config    ; Dirección de la variable para leer
+    	mov rdx, 154                   ; Leer 30 bytes
+	 syscall                       ; Realizar la lectura
 
-contador_config:
-    mov al, byte[var_fetch_config] ; cargo el contador
-    
-  ;  comp al, 4
-  ;  jh cerrar_archivo
+; Verificación de lectura de bytes si no hay cierra el archivo
+    	test rax, rax        ; Verificar si se leyeron bytes
+    	jz  cerrar_archivo   ; Salir si no se leyeron bytes (fin de archivo)
+     
+	mov rsi, leer_texto_config ; cargo los bytes leídos de la linea
+;-------------------------------------------------------------------
+fetch_nota_apro: 
+    	mov eax,[rsi+22]      ; Guardar nota apro
+    	mov [nota_apro], eax
 
+fetch_nota_repo: 
+    	mov eax, [rsi+49]      ; Guardar nota repo
+    	mov [nota_repo],eax
 
-    cmp al, 0   ; es cero?
-    je fetch_nota_apro 
-
-    cmp al, 1   ; es uno?
-    je fetch_nota_repo
-
-    cmp al, 2   ; es dos?
-    je fetch_tam_grupo 
-
-    cmp al, 3   ; es tres?
-    je fetch_escala 
-
-    cmp al, 4   ; es cuatro?
-    je fetch_orden 
-
-    jmp cerrar_archivo
-
-fetch_nota_apro:
-    call captura_dato
-    mov [nota_apro + 1], al  ; Guardar segundo byte
-    mov [nota_apro + 2], bl  ; Guardar tercer byte
-    mov [nota_apro + 3], cl  ; Guardar cuarto byte
-
-    
-    add byte[var_fetch_config],1 ; Avanzar el contador
-    jmp cerrar_archivo
-
-fetch_nota_repo:
-    inc byte[var_fetch_config] ; Avanzar el contador
-    call captura_dato
-    mov [nota_repo + 1], al  ; Guardar segundo byte
-    mov [nota_repo + 2], bl  ; Guardar tercer byte
-    mov [nota_repo + 3], cl  ; Guardar cuarto byte
-    jmp cerrar_archivo
-
-fetch_tam_grupo:
-    inc byte[var_fetch_config]
-    call captura_dato
-    mov [tam_grupo + 1], al  ; Guardar segundo byte
-    mov [tam_grupo + 2], bl  ; Guardar tercer byte
-    mov [tam_grupo + 3], cl  ; Guardar cuarto byte
-    jmp bucle_lectura_byte
+fetch_tam_grupo: 
+    	mov eax, [rsi+87]      ; Guardar tam_grupo
+    	mov [tam_grupo],eax
 
 fetch_escala:
-    inc byte[var_fetch_config]
-    call captura_dato
-    mov [escala + 1], al  ; Guardar segundo byte
-    mov [escala + 2], bl  ; Guardar tercer byte
-    mov [escala + 3], cl  ; Guardar cuarto byte
-    jmp bucle_lectura_byte
+    	mov eax, [rsi+114]      ; Guardar escala
+    	mov [escala], eax
 
 fetch_orden:
-    inc byte[var_fetch_config]
-    call captura_dato
-    mov [orden + 1], al  ; Guardar segundo byte
-    mov [orden + 2], bl  ; Guardar tercer byte
-    mov [orden + 3], cl  ; Guardar cuarto byte
-    jmp cerrar_archivo
+    	mov al, [rsi+133]      ; Guardar orden
+    	mov [orden],al
+;-------------------------------------------------------------
+jmp cerrar_archivo
 
-;-------------------------
-; Función captura_dato
-;-------------------------
-captura_dato:
-    ; Leer 4 bytes
-    mov rdi, rbx         ; Cargar el descriptor del archivo
-    mov rax, SYS_READ    ; Llamada al sistema para leer
-    mov rsi, leer        ; Dirección para almacenar los bytes leídos
-    mov rdx, 4           ; Leer 4 bytes
-    syscall
-    
-    ; Verificación de lectura de bytes
-    test rax, rax        ; Verificar si se leyeron bytes
-    jz cerrar_archivo    ; Salir si no se leyeron bytes (fin de archivo)
 
-filtro:
-    ; Se salta el primer byte por ser un espacio
-    mov al, byte [leer + 1] ; Segundo byte
-
-    ; Verificar si se leyeron dos "saltos de línea" (caso valor menor a 10)
-    mov bl, byte[leer + 2] ; Tercer byte
-    cmp bl, 0x0A ; Comparar con Salto de línea (0x0A)
-    je corregir_salto_linea_dos; Si es, ir a corregir salto línea
-    
-    ; Verificar si se leyó un "salto de línea" (caso valor menor a 100)
-    mov cl, byte[leer + 3] ; Cuarto byte
-    cmp cl, 0x0A ; Salto de línea 0x0A
-    je corregir_salto_linea_uno; Si es, ir a corregir salto línea
-    
-    jmp guardando
-
-corregir_salto_linea_uno:
-    mov cl, 0
-    jmp guardando
-
-corregir_salto_linea_dos:
-    mov bl, 0 ; pone un espacio vacío
-    mov cl, 0
-    
-    jmp guardando
-
-guardando:
-    ret
 
 cerrar_archivo:
-    impresion:
-        print nota_apro
-        ;print nota_repo
-        ; print tam_grupo
-        ; print escala
-        ; print orden
+    ;print nota_repo
+    ;print tam_grupo
+    ;print escala
+    ;print orden
+    ;rint nota_apro
 
     mov rdi, rbx         ; Cargar descriptor del archivo
     mov rax, SYS_CLOSE   ; Llamada para cerrar el archivo
+    syscall
+
+impresionn:
+    mov rdi, 1                 ; fd = 1 (stdout)
+    mov rax, SYS_WRITE         ; syscall: sys_write
+    mov rsi, tam_grupo	       ; Dirección de la cadena a imprimir
+    mov rdx, 3                 ; Longitud de la cadena (número de caracteres)
     syscall
 
 terminar:
