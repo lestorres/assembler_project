64294795cca2b46a843b12102a6c2e93aa4bee8a
lesmes <lesmestorres@estudiantec.cr>
2025-03-28 18:52:51 -0600

    ordenamiento alfabetico listo

--- bubble_sort/bubble_sort.asm
+++ bubble_sort/bubble_sort.asm
@@ -19,7 +19,6 @@ section .data
     msg_final_programa       db "---Programa Terminado---", 0xA, 0
 
     mensaje                  db "             Imprimiendo...", 0xA, 0
-
     nl db 0xA, 0  ; Salto de línea para ordenar
 
 ; Sección de datos no inicializados
@@ -88,6 +87,7 @@ fetch_escala:
     mov eax, [rsi+114]     ; Escala del gráfico
     mov [escala], eax
 
+fetch_orden:
     mov al, [rsi+133]      ; Ordenamiento
     mov [orden], al
 
@@ -119,7 +119,7 @@ fetch_lista_de_datos:
 
     mov rbx, rax  ; carga el descriptor del archivo
 
-    ; Leer el contenido completo del archivo
+;leer el archivo
 lectura_lista:
     mov rax, SYS_READ
     mov rdi, rbx
@@ -144,12 +144,13 @@ lectura_lista:
     syscall
 
 
-ordenamiento:
+ordenamiento_datos:
     print msg_lista_orden_open
     print mensaje
+
 ;------------------estructurar_datos-------------------------------
-Estructurar_datos:
-    ;-- Separar las líneas y almacenar sus direcciones en arreglo_dir_datos --
+estructurar_datos:
+    ;Separar las líneas y almacenar sus direcciones en arreglo_dir_datos --
     mov rsi, leer_texto_datos       ; Puntero al inicio del buffer
     lea rdi, [arreglo_dir_datos]    ; Array para guardar punteros
     xor rcx, rcx                    ; Contador de líneas = 0
@@ -161,44 +162,60 @@ Estructurar_datos:
 
 separa_lineas_loop:
     cmp rsi, rdx                    ; Si se alcanzó el final, salir
-    jae bubble_sort  
-
+    jae comprobar_orden
+    
     ; Guardar la dirección de inicio de la línea actual
     mov qword [arreglo_dir_datos + rcx*8], rsi
     inc rcx
 
-    ; Buscar el final de la línea (LF o CR)
-find_newline:
+    ; Buscar el final de la línea o retorno de carro
+encontrar_linea:
     cmp rsi, rdx
     jae separa_lineas_loop
 
     mov al, [rsi]
-    cmp al, 0x0A
-    je found_newline
-    cmp al, 0x0D
-    je found_newline
+    cmp al, 0x0A ;salto de linea?
+    je nueva_linea_encontrada
+    cmp al, 0x0D ; retorno de carro?
+    je nueva_linea_encontrada
 
     inc rsi
-    jmp find_newline
+    jmp encontrar_linea
 
-found_newline:
+nueva_linea_encontrada:
     ; Poner un caracter 0x00 de fin de línea por nulo y avanzar
     mov byte [rsi], 0
     inc rsi
     jmp separa_lineas_loop
+;---------------------------------------------------------------------------
 
+comprobar_orden:
+    push rax ;preservar el rax en el stack
+    ;para comprobar el bit de orden    
 
+    ;es orden alfabetico?
+    mov al, byte[orden] 
+    cmp al, 0x61	;Revisa si el ordenamiento es una "a" de Alfabetico
+    je bubble_sort_alfabetico
 
-bubble_sort:
-;-----------------Inicio del bubblesort---------------
+    
+    cmp al, 0x41        ;Reovisa si el ordenamiento es una "A" de Alfabetico
+    je bubble_sort_alfabetico
 
-    mov r8, rcx     ; Número total de líneas en r8
-    mov r9, 0       ; Índice i
+    pop rax ; liberar el rax del stack
+
+;Sino es "a" o "alfabetico" se ordena como numerico por notas
+
+bubble_sort_numerico:
+;--------------Inicio del bubblesort numerico--------------
+
+    mov r8, rcx     ; Numero total líneas en r8
+    mov r9, 0       ; indice i
 
 outer_loop:
     cmp r9, r8
-    jge print_sorted_list
-    mov r10, 0      ; Índice j
+    jge print_sorted_notas_lista
+    mov r10, 0      ; indice j
 
 inner_loop:
     mov r11, r8
@@ -207,10 +224,10 @@ inner_loop:
     jge next_outer
 
     ; Cargar punteros de dos líneas consecutivas
-    mov rax, [arreglo_dir_datos + r10*8]      ; Línea actual
-    mov rbx, [arreglo_dir_datos + (r10+1)*8]    ; Siguiente línea
+    mov rax, [arreglo_dir_datos + r10*8]      ; Linea actual
+    mov rbx, [arreglo_dir_datos + (r10+1)*8]    ; línea que sigue
 
-    ; Extraer nota de la línea actual
+    ; Extraer nota de la linea actual
     push r8       ; Preservar r8
     push rcx      ; Preservar contador de líneas
 
@@ -238,7 +255,7 @@ inner_loop:
     mov [arreglo_dir_datos + (r10+1)*8], rdx
 
 no_swap:
-    pop rcx
+    pop rcx ;regresar del stack
     pop r8
     inc r10
     jmp inner_loop
@@ -248,12 +265,12 @@ next_outer:
     jmp outer_loop
 
 ;--------------------Imprimir la lista ordenada-------------------
-print_sorted_list:
+print_sorted_notas_lista:
     mov rsi, 0   ; Índice = 0
 
 print_loop:
     cmp rsi, r8
-    jge terminar          ; Si se han impreso todas las líneas, salir
+    jge fin_n          ; Si se han impreso todas las líneas, salir
 
     ; Obtener el puntero a la línea ordenada e imprimirla
     mov rax, [arreglo_dir_datos + rsi*8]
@@ -263,6 +280,84 @@ print_loop:
     inc rsi
     jmp print_loop
 
+fin_n:
+    jmp terminar   
+;---------------------------------------------------------------------------
+
+
+
+;--------------Inicio del Bubble Sort Alfabético --------------------------
+
+bubble_sort_alfabetico:
+    mov r8, rcx         ; Número total de líneas en r8 (el número de nombres)
+    mov r9, 0           ; Índice i (bucle externo)
+
+outer_loop_a:
+    cmp r9, r8
+    jge imprimir_a
+    mov r10, 0          ; Índice j (bucle interno)
+
+inner_loop_a:
+    mov r11, r8
+    dec r11
+    cmp r10, r11
+    jge next_outer_a
+
+    ; Cargar punteros de dos cadenas consecutivas
+    mov rax, [arreglo_dir_datos + r10*8]    ;Línea actual (nombre actual)
+    mov rbx, [arreglo_dir_datos + (r10+1)*8];Línea siguiente (nombre siguiente)
+
+    ; Cargar la primera letra 
+    mov al, [rax]  ; primera letra de nombre actual
+    mov bl, [rbx]  ; primera letra de nombre siguiente
+    cmp al, bl
+    jg swap_a  ; Si la primera letra de actual es mayor, intercambiar
+    jl no_swap_a ; Si es menor, no intercambiar 
+    
+    ; Si son iguales, comparar la segunda letra
+    mov ah, [rax + 1] ; segunda letra de nombre actual
+    mov bh, [rbx + 1] ; segunda letra de nombre siguiente
+    cmp ah, bh
+    jg swap_a  ; Si la segunda letra de actual es mayor, intercambiar
+    jl no_swap_a ; Si es menor, no intercambiar 
+ 
+no_swap_a:
+    inc r10               ; Incrementar el índice j
+    jmp inner_loop_a      ; Continuar con el siguiente par de cadenas
+
+swap_a:
+    ; Intercambiar si nombre actual debe ir después
+    mov rdx, [arreglo_dir_datos + r10*8]
+    mov rbx, [arreglo_dir_datos + (r10+1)*8]
+    mov [arreglo_dir_datos + r10*8], rbx
+    mov [arreglo_dir_datos + (r10+1)*8], rdx
+
+    jmp inner_loop_a      ; Continuar con el siguiente par de cadenas
+
+next_outer_a:
+    inc r9                ; Incrementar el índice i
+    jmp outer_loop_a      ; Volver al bucle externo
+
+imprimir_a:
+    ; Código para imprimir la lista ordenada
+    mov r10, 0              ; Índice de impresión
+
+print_sorted_list_a:
+    mov rax, [arreglo_dir_datos + r10*8]    ; Cargar puntero de la cadena
+    print rax
+    print nl
+    
+    inc r10 ;incrementa para otra lineac
+    cmp r10, r8
+    jl print_sorted_list_a   ; continuar con otra linea
+
+    ; Fin de la impresión
+    jmp fin_a
+
+fin_a:
+    jmp terminar    
+
+;--------------final del programa---------------------------
 terminar:
     print msg_final_programa    
     mov rax, SYS_EXIT
@@ -276,32 +371,39 @@ salida_error:
 
 ;------------------Subrutinas auxiliares-------------------------------
 
+find_opening_of_string:
 ; find_end_of_string:
 ;   Entrada: r12 = puntero al inicio de la cadena.
-;   Salida: r12 = puntero al carácter nulo (fin de cadena).
+;   Salida: r12 = puntero al carácter nulo (fin de cadena).i
+
 find_end_of_string:
-find_end_loop:
+
+bucle_busca_final:
+;leer hasta encontrar el null de la linea
     cmp byte [r12], 0
     je end_find
     inc r12
-    jmp find_end_loop
-end_find:
+    jmp bucle_busca_final
+encontro_final_linea:
     ret
 
-; ascii_to_int:
-;   Convierte 3 dígitos ASCII (apuntados por r13) a entero.
+; transformae de ascii_to_int:
+;acá transformo los
+;   3 dígitos ASCII (apuntados por r13) a un entero.
 ;   Resultado: r15 = número entero.
 ascii_to_int:
-    ; Primer dígito
-    movzx rax, byte [r13]
-    sub rax, '0'
-    imul rax, 100
-    ; Segundo dígito
+    ; Primer dígito (centecimas)
+    movzx rax, byte [r13]; move with extend zero(pone ceros lo que no se llno)
+    sub rax, '0' ; al restarle 0 se encuentra su valor numerico
+    imul rax, 100 ; multiplica las centenas
+
+    ; Segundo dígito (decimas)
     movzx rcx, byte [r13+1]
     sub rcx, '0'
     imul rcx, 10
     add rax, rcx
-    ; Tercer dígito
+
+    ; Tercer dígito (unidades)
     movzx rcx, byte [r13+2]
     sub rcx, '0'
     add rax, rcx
